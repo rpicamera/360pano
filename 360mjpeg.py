b6eebbc464c88036e9fb15e8009f08ecdd54b479
rpicamera <rpi.camera.studio@gmail.com>
2017-08-30 20:42:40 +0000

    added a mjpeg server

---
+++ 360mjpeg.py
@@ -0,0 +1,124 @@
+#!usrbinpython
+'''
+    Author Igor Maculan - n3wtron@gmail.com
+    A Simple mjpg stream http server
+'''
+import io
+import time
+import picamera
+import cv2
+from PIL import Image
+import threading
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from SocketServer import ThreadingMixIn
+import StringIO
+import numpy as np
+capture=None
+_debug=1
+
+class CamHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.endswith('.mjpg'):
+            self.send_response(200)
+            self.send_header('Content-type','multipartx-mixed-replace; boundary=--jpgboundary')
+            self.end_headers()
+            while True:
+                try:
+                    stream = io.BytesIO()
+                    with picamera.PiCamera() as camera:
+                    	camera.resolution = (342, 256)
+                        camera.capture(stream, format='jpeg')
+                    # Construct a numpy array from the stream
+                    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+                    # "Decode" the image from the array, preserving colour
+                    imgRGB = cv2.imdecode(data, 1)
+                    imgRGB = cv2.remap(imgRGB,xmap,ymap,cv2.INTER_LINEAR)
+                    jpg = Image.fromarray(imgRGB)
+                    tmpFile = StringIO.StringIO()
+                    jpg.save(tmpFile,'JPEG')
+                    self.wfile.write("--jpgboundary")
+                    self.send_header('Content-type','imagejpeg')
+                    self.send_header('Content-length',str(tmpFile.len))
+                    self.end_headers()
+                    jpg.save(self.wfile,'JPEG')
+                    time.sleep(0.05)
+                except KeyboardInterrupt:
+                    break
+            return
+        if self.path.endswith('.html'):
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write('<html><head></head><body>')
+            self.wfile.write('<img src="http://127.0.0.1:8080/cam.mjpg"/>')
+            self.wfile.write('</body></html>')
+            return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """ Handle requests in a separate thread. """
+
+def buildMap(sz_src,sz_out,fov,qvert):
+    if _debug>=1:
+        print("Building map...")
+
+    vfov=fov/180*np.pi
+
+    map_x = np.zeros((sz_out,sz_out*2),np.float32)
+    map_y = np.zeros((sz_out,sz_out*2),np.float32)
+    # vectorizion version
+    if qvert:
+        Phi       = np.pi*(np.arange(float(  sz_out))/float(sz_out-1)-0.5)
+        Theta     = np.pi*(np.arange(float(2*sz_out))/float(sz_out-1)-1)
+        CosPhi    = np.cos(Phi)
+
+        Spx       = np.dot(CosPhi[:].reshape(sz_out,1),np.sin(Theta).reshape(1,2*sz_out))
+        Spy       = np.dot(CosPhi[:].reshape(sz_out,1),np.cos(Theta).reshape(1,2*sz_out))
+        Spz       = np.sin(Phi)
+
+        R         = sz_src * np.arctan(np.sqrt(Spx*Spx+(Spz*Spz)[:].reshape(sz_out,1))/(Spy+1e-20))/vfov
+        # Fixing arctan range 
+        halfOutSz = int(sz_out/2)
+        aPhiMod   = np.append(np.append(np.ones(halfOutSz),np.zeros(sz_out-1)),np.ones(halfOutSz+1))*sz_src*180/fov
+        R         = (R + aPhiMod) * np.append(np.ones(sz_out-1)*-1,np.ones(sz_out+1))   # fixed atan problem
+
+        aTheta    = np.arctan(Spz[:].reshape(sz_out,1)/(Spx+1e-20))
+        map_x     = 0.5*sz_src+(R*np.cos(aTheta)).astype('float32')
+        map_y     = 0.5*sz_src+(R*np.sin(aTheta)).astype('float32')
+
+    else:
+        Phi       = np.pi*(np.arange(float(sz_out))/float(sz_out-1))
+        Theta     = np.pi*(np.arange(float(2*sz_out))/float(sz_out-1)-1)
+        R         = sz_src*Phi/vfov
+
+        map_x     = 0.5*sz_src+(np.dot(R[:].reshape(sz_out,1),np.cos(Theta).reshape(1,2*sz_out))).astype('float32')
+        map_y     = 0.5*sz_src+(np.dot(R[:].reshape(sz_out,1),np.sin(Theta).reshape(1,2*sz_out))).astype('float32')
+
+    return map_x, map_y
+
+def main():
+    global capture
+    global img
+    global xmap,ymap
+    global width
+    global M
+    global mleft
+    global mtop
+
+    mleft = 22
+    mtop = 17
+    width=256
+    M = np.float32([[1,0,0],[0,1,mtop]])
+
+    fov=220
+    [xmap,ymap] = buildMap(width,width,fov,True)
+    try:
+        server = ThreadedHTTPServer(('localhost', 8080), CamHandler)
+        print "server started"
+        server.serve_forever()
+    except KeyboardInterrupt:
+        capture.release()
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
